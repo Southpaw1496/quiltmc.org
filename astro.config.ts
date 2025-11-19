80c2aed677bdf2e322d8fdd2a0c429f0618a1d80
Southpaw <git@southpaw1496.dev>
2025-11-19 16:01:55 +0000

    Test 1

--- astro.config.ts
+++ astro.config.ts
@@ -10,7 +10,7 @@ import icon from "astro-icon"
 import compress from "astro-compress";
 import searchIndex from "./src/integration/search-index";
 
-// https://astro.build/config
+// https://astro.build/config Testing
 export default defineConfig({
   site: "https://quiltmc.org/",
   integrations: [
